77bb2b36f8a0633747de7dfe61fc98db1bd9e01b
Justin Carter <caufield365@yahoo.com>
2018-03-17 09:32:50 -0500

    java add

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+var myArray = [1, 2, 3, 4] //grab specific elements 
+
+var myOjbect = {name: "justin", favoriteColor: "blue"}
+
+var arrayObject = [
+	{name: "justin", favoriteColor: "blue"},
+	{name: "lee", favoriteColor: "green"}
+]
+
+console.log(arrayObject[0].favoriteColor)
+
+
