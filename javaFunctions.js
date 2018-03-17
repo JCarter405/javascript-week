30eec99a5d095343c34dd57401085c7da16b4f4c
Justin Carter <caufield365@yahoo.com>
2018-03-17 10:53:36 -0500

    add more java

--- javaFunctions.js
+++ javaFunctions.js
@@ -7,7 +7,7 @@ function sayhi(name, secondName) {
 console.log(sayhi("Zach", "Justin "))
 
 
-
+// Function type: "For Each"
 
 function forEach(collection, func){
 	for(var i = 0; i < collection.length; i++){
@@ -16,13 +16,26 @@ function forEach(collection, func){
   }	
 }
 
+//forEach([1, 2, 5, 48, 9, 4], function(elem) {
+//	return elem * elem
+//}//)
 
-forEach([1, 2, 5, 48, 9, 4], function(elem) {
+// Function Type: Mapping
+// mapping - container (type: arrat) of elements, length and 
+// making a new collection
+//
+
+
+function map(collection, func) {
+	var mapped = []
+	for(var i = 0; i < collection.length; i++){
+	var result = func(collection[i])
+	mapped.push(result)
+	}
+return mapped	
+}
+
+var result = map([1, 2, 5, 48, 9, 4], function(elem) {
 	return elem * elem
 })
-
-
-
-
-
-
+console.log(result)
